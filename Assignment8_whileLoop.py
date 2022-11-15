24e5b0d2b64121953f33c4a28fad2db558af9e57
Bhawna011 <107423783+Bhawna011@users.noreply.github.com>
2022-11-15 14:16:30 +0530

    Add files via upload
    Assignment 8 While loop is uploaded.

---
+++ Assignment8_whileLoop.py
@@ -0,0 +1,53 @@
+# Assignment - 8  While Loop
+#1. Write a python script to print MySirG 5 times on the screen.
+x=1
+while x<=5:
+    print("MySirG")
+    x=x+1
+#2. Write a python script to print first 10 natural numbers.
+x=1
+while x<=10:
+    print(x,end='')
+    x=x+1
+#3. Write a python script to print first 10 natural numbers in reverse order
+x=1
+while x<=10:
+    print(11-x)
+    x=x+1
+#4. Write a python script to print first 10 odd natural numbers.
+i=1
+while i<=20:
+    print(i)
+    i=i+2
+#5. Write a python script to print first 10 odd natural numbers in reverse order.
+i=19
+while i>=1:
+    print(i)
+    i=i-2
+#6. Write a python script to print first 10 even natural numbers.
+i=2
+while i<=20:
+    print(i)
+    i=i+2
+#7. Write a python script to print first 10 even natural numbers in reverse order
+i=20
+while i>=2:
+    print(i)
+    i=i-2
+#8. Write a python script to print squares of first 10 natural numbers
+x=1
+while x<=10:
+    print(x*x)
+    x=x+1
+#9. Write a python script to print cubes of first 10 natural numbers
+x=1
+while x<=10:
+    print(x*x*x)
+    x=x+1
+#10. Write a python script to print first 10 multiples of 5
+x=1
+while x<=10:
+    print( " 5 x {0} = ".format(x) ,5*x)
+    x=x+1
+print()
+i=5
